3f564faefea79d37b405b25550554ad5d74abe79
Aurus Soares <152621017+neiven13@users.noreply.github.com>
2025-01-22 16:02:15 -0300

    feat: add a fibonacci checker

---
+++ Fibonacci.cs
@@ -0,0 +1,41 @@
+﻿using System;
+public class Fibonacci
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe um número:");
+        if (int.TryParse(Console.ReadLine(), out int numero))
+        {
+            if (PertenceFibonacci(numero))
+            {
+                Console.WriteLine($"O número {numero} pertence à sequência");
+            }
+            else
+                Console.WriteLine($"O número {numero} não pertence à sequência");
+        }
+        else
+        {
+            Console.WriteLine("Insira um número válido.");
+        }
+    }
+
+    static bool PertenceFibonacci(int numero)
+    {
+        if (numero < 0) return false;
+        int anterior = 0;
+        int proximo = 1;
+
+        while (anterior <= numero)
+        {
+            if (anterior == numero) return true;
+
+            int temporario = anterior;
+            anterior = proximo;
+            proximo += temporario;
+        }
+
+        return false;
+    }
+}
+
+
