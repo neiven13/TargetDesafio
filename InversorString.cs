ad8d98e55ea137e62c1e28a4a4abb3c74c7939d0
Aurus Soares <152621017+neiven13@users.noreply.github.com>
2025-01-22 15:56:08 -0300

    feat: add a string inverter program

---
+++ InversorString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class InversorString
+{
+    static void Main()
+    {
+        string textoInicial = "String a ser invertida";
+        string textoInvertido = "";
+
+        for (int i = textoInicial.Length - 1; i >= 0; i--)
+        {
+            textoInvertido += textoInicial[i];
+        }
+
+        Console.WriteLine($"Original: {textoInicial}\nInvertido: {textoInvertido}");
+    }
+}
